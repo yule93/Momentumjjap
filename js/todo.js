d380463ee5f174ae7117171b74e5833b48fadd85
여율 <31907528+yule93@users.noreply.github.com>
2020-08-19 07:22:14 +0000

    fix the error in 'todo.js' and add CSS style to make more simply and modernly ㅋㅋㅋㅋ

--- js/todo.js
+++ js/todo.js
@@ -1,46 +1,42 @@
-const todoForm = document.querySelector(".js-toDoForm"),
-  toDoInput = form.querySelector("input"),
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
+
 let toDos = [];
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-
   const cleanToDos = toDos.filter(function(toDo) {
     return toDo.id !== parseInt(li.id);
   });
-
   toDos = cleanToDos;
   saveToDos();
 }
 
 function saveToDos() {
-  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));  /* stringify는 어떤 변수든 string 타입으로 바꿔준다. javascript는 로컬 저장시 string으로 변환하려는 성질이 있기 때문에 객체인 toDos를 string 타입으로 변환시키지 않는다면 "object"라고만 표기된다. */
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintToDo(text) {
-  const todoLi = document.createElement("li");
+  const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  
   delBtn.innerText = "❌";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
-  todoLi.appendChild(delBtn);
-  todoLi.appendChild(span);
-  todoLi.id = newId;
-  toDoList.appendChild(todoLi);
-
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
   const toDoObj = {
     text: text,
-    id: newId,
+    id: newId
   };
-
   toDos.push(toDoObj);
   saveToDos();
 }
@@ -49,22 +45,22 @@ function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
-  toDoInput.value = ""; 
+  toDoInput.value = "";
 }
 
 function loadToDos() {
-    const loadedToDos = localStorage.getItem(TODOS_LS);
-  if(loadedToDos !== null) {
-    const parsedToDos = JSON.parse(loadedToDos);  // String으로 변환돼 저장된 Object들을 다시 Object로 전환해주는 JavaScript Object Notation 
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(function(toDo) {
-      paintToDo(toDo.text);   // 오브젝트 내 todo-list를 불러와서 보여줌
+      paintToDo(toDo.text);
     });
   }
 }
 
 function init() {
   loadToDos();
-  todoForm.addEventListener("submit", handleSubmit);
+  toDoForm.addEventListener("submit", handleSubmit);
 }
 
 init();
