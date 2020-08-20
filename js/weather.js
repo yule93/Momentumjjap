197d53e74184eb10ae554f418928e8da7681e471
여율 <31907528+yule93@users.noreply.github.com>
2020-08-20 06:26:46 +0000

    update weather function. update css to make it comfortable to see.

--- js/weather.js
+++ js/weather.js
@@ -4,13 +4,14 @@ const API_KEY = "9cc4c16bdce10e08b6a3daa2972d27f1";
 const COORDS = "coords";
 
 function getWeather(lat, lon) {
-  fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
   ).then(function(response){
     return response.json();
   }).then(function(json) {
     const temperature = json.main.temp;
     const place = json.name;
-    weather.innerText = `${temperature} @ ${place}`;
+    weather.innerText = `${temperature}℃, @ ${place}`;
   });
 }
 
